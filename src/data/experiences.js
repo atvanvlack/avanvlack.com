b9886b051483825d27781115036022e76756aeac
Andrew Van Vlack <atvanvlack@gmail.com>
2019-06-12 20:33:27 +0100

    updated site

--- src/data/experiences.js
+++ src/data/experiences.js
@@ -6,7 +6,7 @@ export const experiences = {
     {
       title: 'Solutions Developer - Senior Consultant',
       company: 'Avanade UK & Ireland',
-      description: 'I\'m working as a lead developer at JotForm Source team. We are creating some amazing features for JotForm!',
+      description: 'Currently working as a Solution Developer in London. Avanade is owned by Accenture & Microsoft. The primary focus of my work is in the Microsoft Platform Ecosystem. As a consultant my primary role is to help clients solve highly technical problems.',
       companyLink: 'https://www.avanade.com',
       companyShortDetail: '',
       date: '2018.07 - Present'
@@ -14,7 +14,7 @@ export const experiences = {
     {
       title: 'Full Stack E-Business Developer',
       company: 'American Automobile Association (AAA Club Alliance)',
-      description: 'I\'m using ReactJS and working as a front-end developer to develop new features for JotForm.',
+      description: 'For over 9 years I worked as a Web Developer for AAA. My responsibilities included Web Development, Infrastructure Support and Project Management.',
       companyLink: 'https://midatlantic.aaa.com',
       companyShortDetail: '',
       date: '2009.8 - 2018.5'
@@ -22,9 +22,9 @@ export const experiences = {
     {
       title: 'Network Administrator',
       company: 'Delaware Online',
-      description: 'I was using ASP.Net MVC Framework with N Tier Architecture to develop management systems, learning management systems & big data projects. And i was developing Android / IOS applications using Xamarin or NDK. You can see projects that I have completed for YD Software <a href=\'#projects\'>below.</a>',
-      companyLink: 'https://www.google.com',
-      companyShortDetail: 'Delaware Online',
+      description: 'My first technical job was working as a Network Administrator for a Dial-Up ISP. My responsibilities included everything from Technical Support, Customer Support, Server Administration and Application Administration',
+      companyLink: '/',
+      companyShortDetail: '',
       date: '2001 - 2008.5'
     }
   ]
--- src/data/projects.js
+++ src/data/projects.js
@@ -32,51 +32,6 @@ export const projects = {
         description: 'METU Technopolis Information System ( C# / SQL Server / Xamarin / MongoDB / Web Services)'
       },
       {
-        url: 'http:////yfyi2.odtuteknokent.com.tr',
-        title: 'YFYI',
-        description: 'New Business New Ideas(YFYİ) Entrepreneurship Acceleration Program Management System ( C# / SQL Server / Web Services)'
-      },
-      {
-        url: 'http:////ggj.odtuteknokent.com.tr',
-        title: 'Global Game Jam',
-        description: 'Global Game Jam (METU Technopolis) Management System ( C# / SQL Server / Web Services)'
-      },
-      {
-        url: 'http:////atom.odtuteknokent.com.tr',
-        title: 'ATOM in Games',
-        description: 'ATOM Pre-Incubation Center (METU Technopolis) Management System ( C# / SQL Server / Web Services)'
-      },
-      {
-        url: 'http:////sanayipolitikalari.sanayi.gov.tr',
-        title: 'Sanayi Politikalari Portali',
-        description: 'T.C. Industry Ministry Industrial Policies Portal ( C# / SQL Server / Web Services)'
-      },
-      {
-        url: 'http:////teknojump2.odtuteknokent.com.tr',
-        title: 'TeknoJUMP',
-        description: 'TeknoJUMP Entrepreneurship Acceleration Program Management System ( C# / SQL Server / Web Services)'
-      },
-      {
-        url: 'http:////bigg.odtuteknokent.com.tr',
-        title: 'TUBITAK BIGG / 1512',
-        description: 'TUBITAK Invidual Young Enterprise(BİGG/1512) Program Management System - METU ( C# / SQL Server / Web Services )'
-      },
-      {
-        url: 'http:////aosb.org.tr',
-        title: 'AOSB',
-        description: 'Ankara Chamber of Industry 1st Organized Industrial Zone Web Portal - Mobile Applications (C#, SQL Server, Android SDK - Java / iOS SDK / Web Services)'
-      },
-      {
-        url: '',
-        title: 'AMS App',
-        description: 'Central Bank of the Republic of Turkey (TCMB) Mobile Archive Management System ( Android SDK / Xamarin / Urovo SDK)'
-      },
-      {
-        url: 'http:////yee.org.tr',
-        title: 'YEE Z-BOOK',
-        description: 'Yunus Emre Institute Z-Book Education Platform (C#/ MongoDB/ SQLite, Web Services)'
-      },
-      {
         url: 'http:////evdekiogretmen.bilfen.com',
         title: 'Evdeki Ögretmen(Teacher at Home)',
         description: 'Bilfen Evdeki Ogretmen Online Education System (C# / SQL Server / Web Services)'
